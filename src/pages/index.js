685971af029736c14366d48e9c884726d5cc4005
Kenneth <kenneth.bal@student.ap.be>
2023-05-29 01:14:01 +0200

    attempted api add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import Header from '@/components/Header.js'
 import Slide from '@/components/Slide.js'
-
+import { useState } from 'react'
+import useNetwork from '@/data/network'
 
 export default function Home() {
+  const { network, isLoading, isError } = useNetwork()
+  
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error</div>
+  
+  const stations = network.stations;
+
   return (
-    <>
+    <div className='home'>
       <Header></Header>
       <Slide></Slide>
-    </>
+    </div>
   )
 }
