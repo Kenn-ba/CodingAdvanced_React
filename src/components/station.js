d0eb73c6773a7ceb6f45777b8b1f5634c4d30006
Kenneth <kenneth.bal@student.ap.be>
2023-05-29 18:22:04 +0200

    CSS, extra buttons, responsive

--- src/components/station.js
+++ src/components/station.js
@@ -6,13 +6,17 @@ import StationImage from '@/components/stationImage';
 export default function Station(props) {
   return (
     <Link href={`/stations/${props.station.id}`} className={styles.beacon}>
-      <StationImage station={props.station} />
-      <h2 className={styles.beacon__address}>{props.station.extra.address}</h2>
-      <h3 className={styles.beacon__name}>{props.station.name}</h3> 
-      <div className={styles.beacon__details}>
-        <p className={styles.beacon__amount}>{props.station.free_bikes} available</p>
-        <p className={styles.beacon__distance}>{props.station.distance * 1000}m</p>
+      <StationImage
+        station={props.station}
+        imageClassName={styles.beacon__image} // Custom CSS class for the image
+      />
+      <div className={styles.beacon__content}>
+        <h2 className={styles.beacon__address}>{props.station.extra.address}</h2>
+        <h3 className={styles.beacon__name}>{props.station.name}</h3> 
+          <p className={styles.beacon__amount}>{props.station.free_bikes} available</p>
+          <p className={styles.beacon__distance}>{props.station.distance * 1000}m</p>
       </div>
+      
     </Link>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import { useState, useEffect } from 'react';
 import { getDistance } from '@/utils/getDistance';
 import useNetwork from '@/data/network';
 import StationBeacon from '@/components/station';
+
+import Attach_Money from '@/images/attach_money.png';
+import Person from '@/images/person.png';
+import Warning_Amber from '@/images/warning_amber.png';
+
 
 export default function Home() {
   const [location, setLocation] = useState({});
@@ -18,7 +24,7 @@ export default function Home() {
         (position) => {
           const { latitude, longitude } = position.coords;
 
-          const updatedStations = network?.stations?.map(station => ({
+          const updatedStations = network.stations.map(station => ({
             ...station,
             distance: getDistance(latitude, longitude, station.latitude, station.longitude).distance / 1000
           }));
@@ -59,7 +65,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <div className={styles.body}>
+      <body className={styles.body}>
         <header>
           <h1 className={styles.title}>VELO</h1>
         </header>
@@ -67,8 +73,23 @@ export default function Home() {
           <div className={styles.stationCard}>
             {nearestStations.map(station => <StationBeacon station={station} key={station.id} />)}
           </div>
+
+          <div className={styles.lowerButtons}>
+            <div className={styles.userZone}>
+              <Image src={Person} alt="Description" className={styles.userZone__image} />
+              <h2>User Zone</h2>
+            </div>
+            <div className={styles.rates}>
+              <Image src={Attach_Money} alt="Description" className={styles.rates__image} />
+              <h2>Pricing</h2>
+            </div>
+            <div className={styles.trouble}>
+              <Image src={Warning_Amber} alt="Description" className={styles.trouble__image} />
+              <h2>Notify Issues</h2>
+            </div>
+          </div>
         </main>
-      </div>
+      </body>
     </>
   )
 }
